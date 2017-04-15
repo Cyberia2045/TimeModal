71e57585db7da6d075d876b5d17f2c45f140a807
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-15 16:54:32 -0400

    completed the styling of modal page

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var modal = document.getElementsByClassName("modal")[0];
 var button = document.getElementsByClassName("button")[0];
 var blur = document.getElementsByClassName("blur")[0];
+var closeBox = document.getElementsByClassName("close")[0];
 
 window.onload = function() {
 	setTimeout(modalDisplay, 3000);
@@ -8,10 +9,18 @@ window.onload = function() {
 	function modalDisplay() {
 		modal.style.display = "block"
 		blur.style.display = "block"
-	};
-}; 
 
+	}; // closes modalDisplay()
+}; // closes window.onload
 
+button.addEventListener("click", closeModal);
+closeBox.addEventListener("click", closeModal);
+blur.addEventListener("click", closeModal);
+
+function closeModal() {
+	modal.style.display = "none"
+	blur.style.display = "none"
+}; // closes closeModal()
 
 
 
